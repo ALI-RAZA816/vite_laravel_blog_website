91702610e4ca6fc427016e0391ce88c95c858b67
Ali Raza Mujahid <razadeveloper816@gmail.com>
2026-09-16 12:50:40 +0500

    refactor the sidebar.module.css and sidebar.jsx for responsiveness

--- fronted/src/components/Sidebar.jsx
+++ fronted/src/components/Sidebar.jsx
@@ -34,49 +34,49 @@ const Sidebar = () => {
     <>
         {deleteModel && <DeleteModel/>}
         <div className="container-fluid p-0">
-            <div className="row p-0 g-0">
+            <div className={`row p-0 g-0 ${styles.layoutRow}`}>
                 {sidebarOpen && <div className={styles.overlay} onClick={closeSidebar}></div>}
                 <div className={`col-2 d-flex flex-column ${styles.sidebar} ${styles.sticky} ${sidebarOpen ? styles.open : ''}`}>
-                    {/* Brand */}
-                    <div className={styles.brand}>
-                        <button type="button" className={styles.closeBtn} onClick={closeSidebar} aria-label="Close sidebar">
-                            <BsXLg />
-                        </button>
-                        <h5 className={styles.brandTitle}>
-                        Admin
-                        <br />
-                        Workspace
-                        </h5>
-                        <p className={styles.brandSubtitle}>Management Portal</p>
-                    </div>
+                {/* Brand */}
+                <div className={styles.brand}>
+                    <button type="button" className={styles.closeBtn} onClick={closeSidebar} aria-label="Close sidebar">
+                        <BsXLg />
+                    </button>
+                    <h5 className={styles.brandTitle}>
+                    Admin
+                    <br />
+                    Workspace
+                    </h5>
+                    <p className={styles.brandSubtitle}>Management Portal</p>
+                </div>
 
-                    {/* Nav */}
-                    <ul className={`nav flex-column ${styles.navList}`}>
-                        {navItems.map((item, index) => (
-                        <li className="nav-item" key={index}>
-                            <Link
-                            to={`/admin-panel/${item.label}`}
-                            onClick={closeSidebar}
-                            className={`nav-link text-capitalize d-flex align-items-center ${styles.navLink} ${
-                                location.pathname === `/admin-panel/${item.label}` ? styles.active : ""
-                            }`}
-                            >
-                            <span className={styles.icon}>{item.icon}</span>
-                            <span>{item.label}</span>
-                            </Link>
-                        </li>
-                        ))}
-                    </ul>
+                {/* Nav */}
+                <ul className={`nav flex-column ${styles.navList}`}>
+                    {navItems.map((item, index) => (
+                    <li className="nav-item" key={index}>
+                        <Link
+                        to={`/admin-panel/${item.label}`}
+                        onClick={closeSidebar}
+                        className={`nav-link text-capitalize d-flex align-items-center ${styles.navLink} ${
+                            location.pathname === `/admin-panel/${item.label}` ? styles.active : ""
+                        }`}
+                        >
+                        <span className={styles.icon}>{item.icon}</span>
+                        <span>{item.label}</span>
+                        </Link>
+                    </li>
+                    ))}
+                </ul>
 
-                    {/* Logout */}
-                    <div className={styles.logoutWrapper}>
-                        <a href="#" className={`d-flex align-items-center ${styles.logoutLink}`}>
-                        <span className={styles.icon}>
-                            <BsBoxArrowRight />
-                        </span>
-                        <span>Logout</span>
-                        </a>
-                    </div>
+                {/* Logout */}
+                <div className={styles.logoutWrapper}>
+                    <a href="#" className={`d-flex align-items-center ${styles.logoutLink}`}>
+                    <span className={styles.icon}>
+                        <BsBoxArrowRight />
+                    </span>
+                    <span>Logout</span>
+                    </a>
+                </div>
                 </div>
                 <div className={`col-10 p-0 ${styles.content}`}>
                     <Outlet/>
